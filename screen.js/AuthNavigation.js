c7fe59796235b58db7566107cc1ce10e80ea2358
Yuvarani-Balbhas <56103835+Yuvarani-Balbhas@users.noreply.github.com>
2019-12-11 17:42:53 +0530

    Update AuthNavigation.js

--- screen.js/AuthNavigation.js
+++ screen.js/AuthNavigation.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { createStackNavigator } from 'react-navigation-stack'
+import Login from '../screens/Login'
+import Signup from '../screens/Signup'
+import ForgotPassword from '../screens/ForgotPassword'
 
-class ForgotPassword extends Component {
-  render() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Forgot Password Screen</Text>
-      </View>
-    )
+const AuthNavigation = createStackNavigator(
+  {
+    Login: { screen: Login },
+    Signup: { screen: Signup },
+    ForgotPassword: { screen: ForgotPassword }
+  },
+  {
+    initialRouteName: 'Login',
+    headerMode: 'none'
   }
-}
+)
 
-export default ForgotPassword
+export default AuthNavigation
